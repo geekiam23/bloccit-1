131d8c7e9e2a626629ade098ca78c2ea6750729c
Gerardo Manllo <gerardomanllop@gmail.com>
2019-02-25 16:58:49 -0600

    MVC checkpoint completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -8,25 +8,16 @@ describe("routes : static", () => {
     describe("GET /", () => {
 
         //#2
-        it("should return status code 200", (done) => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
             //#3
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-
+                expect(body).toContain("Welcome to Bloccit");
                 //#4
                 done();
             });
         });
 
     });
-
-    describe("GET /marco", () => {
-        it("should return status code 200", (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-                done();
-            })
-      })
-    })
 });
