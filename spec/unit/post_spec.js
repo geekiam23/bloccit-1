21f3c9034f27a3a97ffdb3d4e8465080fb982594
Gerardo Manllo <gerardomanllop@gmail.com>
2019-03-25 12:09:12 -0600

    Validations done

--- spec/unit/post_spec.js
+++ spec/unit/post_spec.js
@@ -120,5 +120,31 @@ describe("Post", () => {
 
     });
 
+    it("should not create a new post that fails validations", (done) => {
+        const options = {
+            url: `${base}/${this.topic.id}/posts/create`,
+            form: {
+                //#1
+                title: "a",
+                body: "b"
+            }
+        };
+
+        request.post(options,
+            (err, res, body) => {
+
+                //#2
+                Post.findOne({ where: { title: "a" } })
+                    .then((post) => {
+                        expect(post).toBeNull();
+                        done();
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        done();
+                    });
+            }
+        );
+    });
 
 });
